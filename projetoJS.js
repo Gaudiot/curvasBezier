bbb47fa3d46b189151ca44f94a6fa924d9d2cbfd
victor <victor.gaudiot@gmail.com>
2019-11-14 01:26:01 -0300

    agora pode botar novas curvas

--- projetoJS.js
+++ projetoJS.js
@@ -1,32 +1,22 @@
 var mainCanvas = document.getElementById("mainCanvas");
 
 //informacoes sobre a curva
+var selCurve = 0;
 var qttCurves = 0;
 var beziers = new Array();
 
-function controlPoint(x, y){
+//Objeto coordenada
+function coord(x, y){
 	this.x = x;
 	this.y = y;
 }
 
-function curve(qttPoints){
-	this.qttPoints = qttPoints;
-	this.points = new Array();
-	for(let i = 0 ; i < qttPoints ; i++){
-		points[i] = new controlPoint(0, 0);
-	}
-}
-
+//Funcao para adicionar curva
 function addCurve(){
-	var qttPoints;
-	do{
-		qttPoints = prompt("Insira a quantidade inicial de pontos de controle.\n O número deve ser um inteiro maior do que 1.");
-	}while(isNaN(qttPoints));
-	if(qttPoints == null){
-		alert("Criação de curva cancelada.");
-	}else{
-		qttCurves++;
-	}
+	qttCurves++;
+	selCurve = (selCurve+1)%qttCurves;
+
+	beziers[qttCurves-1] = new Array();
 }
 
 function deleteCurve(){
@@ -39,6 +29,10 @@ function deleteCurve(){
 
 var x, y, qttPoints = 0;
 function addPoint(event){
+	if(qttCurves == 0){
+		alert("Criei uma curva par adicionar pontos.")
+		return;
+	}
 	var web = "http://www.w3.org/2000/svg";
 	var mouseX = event.offsetX, mouseY = event.offsetY;
 	var c = document.createElementNS(web, "circle");
@@ -47,20 +41,35 @@ function addPoint(event){
 	c.setAttribute("r", "6");
 	c.setAttribute("fill", "green");
 	mainCanvas.appendChild(c);
-	if(qttPoints > 0){
+
+	//Criacao de linhas
+	if(beziers[selCurve].length > 0){
 		var l = document.createElementNS(web, "line");
-		l.setAttribute("x1", x);
-		l.setAttribute("y1", y);
+		l.setAttribute("x1", beziers[selCurve][beziers[selCurve].length-1].x);
+		l.setAttribute("y1", beziers[selCurve][beziers[selCurve].length-1].y);
 		l.setAttribute('x2', mouseX);
 		l.setAttribute('y2', mouseY);
 		l.setAttribute('style', 'stroke : #000000; stroke-width : 2');
 		mainCanvas.appendChild(l);
 	}
-	qttPoints++;
-	x = mouseX;
-	y = mouseY;
+
+	//Adicionar novo ponto ao seu objeto bezier
+	beziers[selCurve][beziers[selCurve].length] = new coord(mouseX, mouseY);
+	//alert(beziers[selCurve].length);
 }
 
+//Funcao para ir para a proxima curva
+function changeCurve(direction){
+	if(selCurve == qttCurves-1 && direction == 1){
+		selCurve = 0;
+	}else if(selCurve == 0 && direction == -1){
+		selCurve = qttCurves-1;
+	}else{
+		selCurve = (selCurve + direction);
+	}
+}
+
+//Funcao para mostrar/esconder pontos de controle
 var pointsVisible = true;
 function showPoints(){
 	var points = document.getElementsByTagName("circle");
@@ -76,6 +85,8 @@ function showPoints(){
 	}
 }
 
+
+//Funcao para mostrar/esconder poligonais de controle
 var linesVisible = true;
 function showLines(){
 	var lines = document.getElementsByTagName("line");
@@ -90,6 +101,11 @@ function showLines(){
 
 		}
 	}
+}
+
+//Funcao para mostrar qual a curva selecionada
+function selectCurve(){
+
 }
 
 function show(){
