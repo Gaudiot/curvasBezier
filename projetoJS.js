557261059558f41c0ba828f8e0e8b79a6f373f81
victor <victor.gaudiot@gmail.com>
2019-11-14 21:24:57 -0300

    finalizado delete e selecao(eu espero)

--- projetoJS.js
+++ projetoJS.js
@@ -1,4 +1,7 @@
 var mainCanvas = document.getElementById("mainCanvas");
+var pointCB = document.getElementById("pointCB");
+var lineCB = document.getElementById("lineCB");
+var curveCB = document.getElementById("curveCB");
 
 //informacoes sobre a curva
 var selCurve = 0;
@@ -14,7 +17,8 @@ function coord(x, y){
 //Funcao para adicionar curva
 function addCurve(){
 	qttCurves++;
-	selCurve = (selCurve+1)%qttCurves;
+	unselectCurve();
+	selCurve = qttCurves-1;
 
 	beziers[qttCurves-1] = new Array();
 }
@@ -22,34 +26,79 @@ function addCurve(){
 function deleteCurve(){
 	if(qttCurves > 0){
 		qttCurves--;
+		//Removecao dos pontos e linhas da curva
+		let lineClass = "l" + selCurve;
+		var lines = document.getElementsByClassName(lineClass);
+		for (var i = lines.length - 1; i >= 0; i--) {
+			mainCanvas.removeChild(lines[i]);
+		}
+		let pointClass = "p" + selCurve;
+		var points = document.getElementsByClassName(pointClass);
+		for (var i = points.length - 1; i >= 0; i--) {
+			mainCanvas.removeChild(points[i]);
+		}
+
+		//Realocacao da ultima curva
+		lineClass = "l" + qttCurves;
+		let newLineClass = "l" + selCurve;
+		var lines = document.getElementsByClassName(lineClass);
+		for (var i = lines.length - 1; i >= 0; i--) {
+			lines[i].setAttribute("class", newLineClass);
+		}
+		pointClass = "p" + qttCurves;
+		let newPointClass = "p" + selCurve;
+		var points = document.getElementsByClassName(pointClass);
+		for (var i = points.length - 1; i >= 0; i--) {
+			points[i].setAttribute("class", newPointClass)
+		}
+
+		beziers = beziers.slice(0, selCurve+1).concat(beziers.slice(selCurve + 1));
+
+		selCurve = qttCurves-1;
+		selectCurve();
+
 	}else{
 		alert("Não há mais curvas para deletar.");
 	}
 }
 
-var x, y, qttPoints = 0;
+//Adiciona um ponto a tela
 function addPoint(event){
+	//Nao permite adicinoar pontos se nao houver linhas
 	if(qttCurves == 0){
-		alert("Criei uma curva par adicionar pontos.")
+		alert("Crie uma curva para adicionar pontos.")
 		return;
 	}
+
 	var web = "http://www.w3.org/2000/svg";
 	var mouseX = event.offsetX, mouseY = event.offsetY;
+	let pointClass = "p" + selCurve;
+
+	//Criacao de pontos
+	let color;
+	if(pointCB.checked) color = "green";
+	else color = "transparent";
 	var c = document.createElementNS(web, "circle");
 	c.setAttribute("cx", mouseX);
 	c.setAttribute("cy", mouseY);
 	c.setAttribute("r", "6");
-	c.setAttribute("fill", "green");
+	c.setAttribute("fill", color);
+	c.setAttribute("class", pointClass);
 	mainCanvas.appendChild(c);
 
 	//Criacao de linhas
 	if(beziers[selCurve].length > 0){
+		let color;
+		let lineClass = "l" + selCurve;
+		if(lineCB.checked) color = "#a832a0";
+		else color = "#00000000"
 		var l = document.createElementNS(web, "line");
 		l.setAttribute("x1", beziers[selCurve][beziers[selCurve].length-1].x);
 		l.setAttribute("y1", beziers[selCurve][beziers[selCurve].length-1].y);
 		l.setAttribute('x2', mouseX);
 		l.setAttribute('y2', mouseY);
-		l.setAttribute('style', 'stroke : #000000; stroke-width : 2');
+		l.setAttribute('style', 'stroke : ' + color + '; stroke-width : 2');
+		l.setAttribute("class", lineClass)
 		mainCanvas.appendChild(l);
 	}
 
@@ -60,44 +109,42 @@ function addPoint(event){
 
 //Funcao para ir para a proxima curva
 function changeCurve(direction){
-	if(selCurve == qttCurves-1 && direction == 1){
-		selCurve = 0;
-	}else if(selCurve == 0 && direction == -1){
-		selCurve = qttCurves-1;
-	}else{
-		selCurve = (selCurve + direction);
+	unselectCurve();
+	if(direction == 1){
+		selCurve = (selCurve+1)%qttCurves;
+	}else if(direction == -1){
+		selCurve = (selCurve + qttCurves - 1)%qttCurves;
 	}
+	selectCurve();
 }
 
 //Funcao para mostrar/esconder pontos de controle
-var pointsVisible = true;
 function showPoints(){
 	var points = document.getElementsByTagName("circle");
-	pointsVisible = !pointsVisible;
-	if(pointsVisible){
+	if(pointCB.checked){
 		for (var i = points.length - 1; i >= 0; i--) {
 			points[i].setAttribute("fill", "black");
 		}
+		selectCurve();
 	}else{
 		for (var i = points.length - 1; i >= 0; i--) {
-			points[i].setAttribute("fill", "white");
+			points[i].setAttribute("fill", "transparent");
 		}
 	}
 }
 
 
 //Funcao para mostrar/esconder poligonais de controle
-var linesVisible = true;
 function showLines(){
 	var lines = document.getElementsByTagName("line");
-	linesVisible = !linesVisible;
-	if(linesVisible){
+	if(lineCB.checked){
 		for (var i = lines.length - 1; i >= 0; i--) {
 			lines[i].setAttribute("style", 'stroke : #000000; stroke-width : 2');
 		}
+		selectCurve();
 	}else{
 		for (var i = lines.length - 1; i >= 0; i--) {
-			lines[i].removeAttribute("style");
+			lines[i].setAttribute("style", 'stroke : #00000000; stroke-width : 2');
 
 		}
 	}
@@ -105,7 +152,42 @@ function showLines(){
 
 //Funcao para mostrar qual a curva selecionada
 function selectCurve(){
+	//seleciona todos os pontos da curva atual e as torna verde
+	if(pointCB.checked){
+		let pointClass = "p" + selCurve;
+		var points = document.getElementsByClassName(pointClass);
+		for (var i = points.length - 1; i >= 0; i--) {
+			points[i].setAttribute("fill", "green");
+		}
+	}
+	//seleciona todas as linhas da curva atual e as torna roxa
+	if(lineCB.checked){
+		let lineClass = "l" + selCurve;
+		var lines = document.getElementsByClassName(lineClass);
+		for (var i = lines.length - 1; i >= 0; i--) {
+			lines[i].setAttribute("style", 'stroke : #a832a0 ; stroke-width : 2');
+		}
+	}
+}
 
+//Funcao para deselecionar a curva
+function unselectCurve(){
+	//seleciona todos os pontos da curva atual e as torna pretas
+	if(pointCB.checked){
+		let pointClass = "p" + selCurve;
+		var points = document.getElementsByClassName(pointClass);
+		for (var i = points.length - 1; i >= 0; i--) {
+			points[i].setAttribute("fill", "black");
+		}
+	}
+	//seleciona todas as linhas da curva atual e as torna pretas
+	if(lineCB.checked){
+		let lineClass = "l" + selCurve;
+		var lines = document.getElementsByClassName(lineClass);
+		for (var i = lines.length - 1; i >= 0; i--) {
+			lines[i].setAttribute("style", 'stroke : #000000 ; stroke-width : 2');
+		}
+	}
 }
 
 function show(){
